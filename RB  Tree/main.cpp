634ab1710ff4ac6b0e909c40a6854108c63304e5
MeowOhMeow <abc22681157@gmail.com>
2023-10-17 22:42:23 +0800

    use value to del

--- RB  Tree/main.cpp
+++ RB  Tree/main.cpp
@@ -4,12 +4,18 @@
 
 using namespace std;
 
-void del(RBTree &tree, vector<Node *> &array, int toBeDeleted)
+void del(RBTree &tree, vector<Node *> &array, int value)
 {
-    vector<Node *>::iterator it = array.begin() + toBeDeleted;
-    tree.del(*it);
-    delete *it;
-    array.erase(it);
+    for (int i = 0; i < array.size(); i++)
+    {
+        if (array[i]->key == value)
+        {
+            tree.del(array[i]);
+            delete array[i];
+            array.erase(array.begin() + i);
+            break;
+        }
+    }
 
     cout << "Structure:\n";
     tree.printStructure();
@@ -35,10 +41,10 @@ int main()
     cout << endl;
 
     cout << "Deleting:\n";
-    del(tree, array, 5);
-    del(tree, array, 3);
-    del(tree, array, 3);
-    del(tree, array, 2);
-    del(tree, array, 1);
-    del(tree, array, 0);
+    del(tree, array, 8);
+    del(tree, array, 12);
+    del(tree, array, 19);
+    del(tree, array, 31);
+    del(tree, array, 38);
+    del(tree, array, 41);
 }
