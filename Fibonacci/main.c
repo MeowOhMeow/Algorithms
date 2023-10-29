87857e411651f41c92156d91470e21145d9cf733
MeowOhMeow <abc22681157@gmail.com>
2023-10-29 22:08:17 +0800

    not using any thing too excessive for riscv

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int size = 10;
-int *solved;
-
-void fibo(int n)
+int main(int argc, char *argv[])
 {
-    for (int i = 0; i < n; i++)
+    int size = 10;
+    int *solved = (int *)malloc(sizeof(int) * size);
+    for (int i = 0; i < size; i++)
     {
         if (i == 0)
         {
@@ -21,13 +17,6 @@ void fibo(int n)
             solved[i] = solved[i - 1] + solved[i - 2];
         }
     }
-}
 
-int main(int argc, char *argv[])
-{
-    solved = (int *)malloc(sizeof(int) * size);
-    fibo(size);
-    printf("%d\n", solved[size - 1]);
-
-    return EXIT_SUCCESS;
+    return 0;
 }
