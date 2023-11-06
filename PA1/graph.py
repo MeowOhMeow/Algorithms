96b0cc41a9ec4f32a099a81b9e0459f518ce3bc1
MeowOhMeow <abc22681157@gmail.com>
2023-11-06 15:33:05 +0800

    visualization

--- PA1/graph.py
+++ PA1/graph.py
@@ -1,31 +1,38 @@
 import matplotlib.pyplot as plt
 import math
+from tkinter.filedialog import askopenfilename
+import os
 
 def get_line_loc(num_points, first, second):
-    rad1 = first/num_points
-    rad2 = second/num_points
+    rad1 = first/num_points*math.pi*2
+    rad2 = second/num_points*math.pi*2
     x1 = math.cos(rad1)
     y1 = math.sin(rad1)
     x2 = math.cos(rad2)
     y2 = math.sin(rad2)
     return (x1, y1, x2, y2)
 
-with open(r'D:\\Programming\\C++\\Algorithms\\PA1\\io\\out\\5000.out', 'r') as f:
-    data = f.readlines()
-    num_points = int(data[0])
-    lines = []
-    for i in range(1, len(data)):
-        line = data[i].split()
-        lines.append((int(line[0]), int(line[1])))
-    
-    plt.figure(figsize=(8, 8))
-    plt.axis('off')
-    plt.xlim(-1.1, 1.1)
-    plt.ylim(-1.1, 1.1)
-    for i in range(len(lines)):
-        line = get_line_loc(num_points, lines[i][0], lines[i][1])
-        plt.plot([line[0], line[2]], [line[1], line[3]], 'b')
-    plt.show()
-    
+path = askopenfilename(initialdir=os.getcwd(), title='Select file', filetypes=(('Text files', '*.out'), ('All files', '*.*')))
+
+if path == '':
+    print('No file selected')
+else:
+    with open(path, 'r') as f:
+        data = f.readlines()
+        num_lines = int(path.split('/')[-1].split('.')[0])
+        lines = []
+        for i in range(1, len(data)):
+            line = data[i].split()
+            lines.append((int(line[0]), int(line[1])))
+        
+        plt.figure(figsize=(8, 8))
+        plt.axis('off')
+        plt.xlim(-1.1, 1.1)
+        plt.ylim(-1.1, 1.1)
+        for i in range(len(lines)):
+            line = get_line_loc(num_lines, lines[i][0], lines[i][1])
+            plt.plot([line[0], line[2]], [line[1], line[3]], 'b')
+        plt.show()
+        
 
 
