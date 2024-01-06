16ce40fb29e85165f79ada7cd536af6c5a45b617
MeowOhMeow <abc22681157@gmail.com>
2024-01-06 17:59:21 +0800

    use template

--- Insertion Sort/insertionSort.h
+++ Insertion Sort/insertionSort.h
@@ -1,19 +1,58 @@
 #ifndef INSERTIONSORT_H
 #define INSERTIONSORT_H
 
-#include "result.h"
 #include <vector>
-#include <chrono>
+#include <cstddef>
 
-using namespace std;
-
-class InsertionSort
+// for dynamic arrays
+template <typename T>
+void sort(T* array, size_t size)
 {
-private:
-    chrono::high_resolution_clock::time_point start;
+    for (size_t i = 1; i < size; ++i)
+    {
+        T key = array[i];
+        int j = i - 1;
+        while (j >= 0 && array[j] > key)
+        {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = key;
+    }
+}
 
-public:
-    Result sort(vector<int> &array);
-};
+// for static arrays
+template <typename T, size_t N>
+void sort(T (&array)[N])
+{
+    for (size_t i = 1; i < N; ++i)
+    {
+        T key = array[i];
+        int j = i - 1;
+        while (j >= 0 && array[j] > key)
+        {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = key;
+    }
+}
 
-#endif /* insertionSort_h */
+// for vectors
+template <typename T>
+void sort(std::vector<T>& vec)
+{
+    for (size_t i = 1; i < vec.size(); ++i)
+    {
+        T key = vec[i];
+        int j = i - 1;
+        while (j >= 0 && vec[j] > key)
+        {
+            vec[j + 1] = vec[j];
+            --j;
+        }
+        vec[j + 1] = key;
+    }
+}
+
+#endif // INSERTIONSORT_H
--- Insertion Sort/main.cpp
+++ Insertion Sort/main.cpp
@@ -1,19 +1,44 @@
-#include "insertionSort.h"
-#include "result.h"
+#include <iostream>
+#include <vector>
 #include <random>
+
+#include "InsertionSort.h"
+
+using namespace std;
 
 int main()
 {
-    int size = 10;
-    vector<int> array(size);
-    for (int i = 0; i < size; i++)
+    int array[] = { 5, 2, 4, 6, 1, 3 };
+    sort(array);
+    for (int val : array)
     {
-        array[i] = rand() % size;
+        cout << val << " ";
     }
+    cout << endl;
 
-    InsertionSort sorter;
-    Result result = sorter.sort(array);
-    result.print();
+    vector<int> vec = { 5, 2, 4, 6, 1, 3 };
+    sort(vec);
+    for (int val : vec)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    int size = 10;
+    int* dynamicArray = new int[size];
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 100);
+    for (int i = 0; i < size; ++i)
+    {
+        dynamicArray[i] = dis(gen);
+    }
+    sort(dynamicArray, size);
+    for (int i = 0; i < size; ++i)
+    {
+        cout << dynamicArray[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
